97cfb4c3dc2182767539b1e659f57f5522c396b3
Ulbolsyn <uliana2596@gmail.com>
2022-11-23 10:45:21 +0600

    Add files via upload

--- report.php
+++ report.php
@@ -3,15 +3,28 @@
     include('connect.php');
     
     class PDF extends FPDF {
-        
+        function Header()
+            {
+                
+                $this->SetFont('Times','',12);
+                $this->Cell(80);
+                $this->Cell(30,10,'Report',1,0,'C');
+                $this->Ln(20);
+            }
+            function Footer()
+        {
+            $this->SetY(-15);
+            $this->SetFont('Times','',12);
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+        }
     }
 
-    $result = mysqli_query($connection, "SELECT * FROM `users`");
+    $result = mysqli_query($connection, "SELECT `id`,`iin`,`goal`, `date`, `time`, `operation` FROM `users`");
 
     $pdf = new PDF();
     $pdf->AliasNbPAges();
     $pdf->AddPage();
-    $pdf->SetFont('helvetica','',10);
+    $pdf->SetFont('Times','',12);
     foreach($result as $row) {
         $pdf->Ln();
         foreach($row as $column)
